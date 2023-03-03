7ee170c96200a5eda29f52085e583b5167fbe56d
Jacksonckc <jackson858216047@gmail.com>
2023-03-03 14:16:21 -0700

    first commit

---
+++ backend/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+
+module.exports = class Database {
+  init = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected successfully to server');
+  };
+};
---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const Express = require('express');
+const Database = require('./database');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const dotenv = require('dotenv');
