5e967979a368b69b66e66673faf33967671f52ae
Jacksonckc <jackson858216047@gmail.com>
2023-03-03 18:18:22 -0700

    1

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,12 +2,12 @@ const routes = require('express').Router();
 const controllers = require('../controllers');
 const { getAuth } = require('../middleware/auth');
 
-routes.get('/', getAuth, controllers.getUser); // Only available when you have the token
+routes.get('/', getAuth, controllers.getUser);
 routes.post('/', controllers.addUser);
-routes.put('/', getAuth, controllers.changeUserInfo); // this will still need to be changed, the valiation
+routes.put('/', getAuth, controllers.changeUserInfo);
 routes.delete('/', getAuth, controllers.deleteUser);
 routes.put('/password', getAuth, controllers.updateUserPassword);
-routes.post('/login', controllers.loginUser); // This will return a token to the frontend
-routes.get('/all', getAuth, controllers.getUsers); // This should be a protective path for only admins
+routes.post('/login', controllers.loginUser);
+routes.get('/all', getAuth, controllers.getUsers);
 
 module.exports = routes;
